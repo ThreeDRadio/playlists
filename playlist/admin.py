a8509deaf5dc3a1a63bf72a74059eb3c6d602a29
Michael Marner <michael@20papercups.net>
2015-09-30 08:31:29 +0930

    Simple playlists in admin.

--- playlist/admin.py
+++ playlist/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Playlist, PlaylistEntry
+
 # Register your models here.
+class PlaylistEntryInline(admin.TabularInline):
+    list_display = ['artist','title']
+    exclude = ['catalogueEntry']
+    model = PlaylistEntry 
+
+class PlaylistAdmin(admin.ModelAdmin):
+    inlines = [PlaylistEntryInline]
+
+admin.site.register(Playlist, PlaylistAdmin)
