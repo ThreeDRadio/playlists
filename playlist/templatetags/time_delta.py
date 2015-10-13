7bbcb714d417807c8776a58292a08734bd6cb06b
Michael Marner <michael@20papercups.net>
2015-10-13 19:58:19 +1030

    Gracefully handle non timedelta objects in the template tags.
    Existing database has missing data.
    Including test cases!

--- playlist/templatetags/time_delta.py
+++ playlist/templatetags/time_delta.py
@@ -1,19 +1,21 @@
 from django import template
+from datetime import timedelta
 
 register = template.Library()
 
 def time_delta(value):
-    toReturn = ""
-    seconds = value.total_seconds()
-    
-    hours = int(seconds // (60*60))
-    minutes = int((seconds- hours*60*60) // 60)
-    second = int(seconds % 60)
-    if hours > 0:
-        return {'0'}.format(hours,minute,second)
+    if isinstance(value, timedelta):
+        seconds = value.total_seconds()
+        
+        hours = int(seconds // (60*60))
+        minutes = int((seconds- hours*60*60) // 60)
+        second = int(seconds % 60)
+        if hours > 0:
+            return '{:01d}:{:02d}:{:02d}'.format(hours, minutes,second)
 
-    return '{:01d}:{:02d}'.format(minutes,second)
-
+        return '{:01d}:{:02d}'.format(minutes,second)
+    else:
+        return '0:00'
 
 
 register.filter('time_delta', time_delta)
