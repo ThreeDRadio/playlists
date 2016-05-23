c51d1c3ff8607e8010b70da0fcf9ab21b6bbf7e6
Michael Marner <michael@20papercups.net>
2016-05-23 09:32:18 +0930

    Fix chart on shows page, thanks to API changes.

--- playlist/static/js/show-chart.js
+++ playlist/static/js/show-chart.js
@@ -1,3 +1,4 @@
 Chart.defaults.global.responsive = true;
+Chart.defaults.global.legend.display =false
 var ctx = document.getElementById("showStats").getContext("2d");
-var statsChart = new Chart(ctx).Bar(statsData, {});
+var statsChart = new Chart(ctx, {type: 'bar', data: statsData, label: '', scaleStartValue: 0});
