4c2764f1bd2df5ea95517e5e857a11627df6880a
Michael Marner <michael@20papercups.net>
2016-06-01 12:03:06 +0930

    Add show attributes.

--- frontend/app/models/show.js
+++ frontend/app/models/show.js
@@ -1,5 +1,10 @@
 import Model from 'ember-data/model';
 
 export default Model.extend({
-  
+
+    name: attr('string'),
+    defaultHost: attr('string'),
+    startTime: attr('string'),
+    endTime: attr('string'),
+    playlists: hasMany('playlist')
 });
