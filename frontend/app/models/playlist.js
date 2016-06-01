5180275039c120a67b71ccf19dbc1d58d24aaa54
Michael Marner <michael@20papercups.net>
2016-06-01 12:03:44 +0930

    Add attributes to playlist.

--- frontend/app/models/playlist.js
+++ frontend/app/models/playlist.js
@@ -1,5 +1,13 @@
 import Model from 'ember-data/model';
 
+
 export default Model.extend({
+
+    show: belongsTo('show'),
+    host: attr('string'),
+    date: attr('date'),
+    notes: attr('string'),
+    complete: attr('boolean'),
+    tracks: hasMany('playlistentry')
   
 });
