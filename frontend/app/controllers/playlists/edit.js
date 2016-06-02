0e0adcbf638425a0e9bd1acfa767da4ec9c404f5
Michael Marner <michael@20papercups.net>
2016-06-02 21:12:35 +0930

    Can now add tracks to end of playlist.
    * Perfect for working on the job.

---
+++ frontend/app/controllers/playlists/edit.js
@@ -0,0 +1,41 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+    isValidTime(v) {
+        if (v === "")
+            return true;
+        return true;
+    },
+
+    actions: {
+        submitTrack() {
+            let artist = this.getWithDefault('artist', "").trim();
+            let track  = this.getWithDefault('track', "").trim();
+            let album  = this.getWithDefault('album', "").trim();
+            let duration = this.getWithDefault('duration', "").trim()
+
+            if (artist.length > 0 && track.length > 0 && album.length > 0 && this.isValidTime(duration)) {
+                let entry = this.store.createRecord('playlistentry', {
+                    artist: artist,
+                    title: track,
+                    album: album,
+                    duration: duration, 
+                    local: this.getWithDefault('local', false),
+                    australian: this.getWithDefault('australian', false),
+                    female: this.getWithDefault('female', false),
+                    newRelease: this.getWithDefault('newRelease', false),
+                    playlist: this.model
+                    });
+                    entry.save().then( function() {
+                        window.scrollTo(0, document.body.scrollHeight);
+                    });
+                this.setProperties({artist: '', track: '', album: '', duration: '', local: false, australian: false, female: false, newRelease: false});
+                
+            }
+            else {
+
+            }
+        }
+    }
+});
