d5d185ebcc1554229c7fa8fbaaaddb34f640ec2a
Michael Marner <michael@20papercups.net>
2016-06-03 16:35:35 +0930

    Add JQuery UI.

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -25,6 +25,7 @@ module.exports = function(defaults) {
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
   app.import('bower_components/moment/moment.js');
   app.import('bower_components/jquery.floatThead/dist/jquery.floatThead.js');
+  app.import('bower_components/jquery-ui/jquery-ui.js');
   app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {destDir: '/fonts'});
   return app.toTree();
 };
