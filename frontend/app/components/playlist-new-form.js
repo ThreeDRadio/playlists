f79f844a992508d168879d3610700e730697d2be
Michael Marner <michael@20papercups.net>
2016-06-03 19:38:50 +0930

    UI working for new playlist form, just need the action.
    Why does ember need routes, controllers, and components? Seems to me like route
    and controllers is unnecessary separation, but I'm happy to be corrected.

---
+++ frontend/app/components/playlist-new-form.js
@@ -0,0 +1,38 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+
+    didInsertElement() {
+        $('.date-picker').datepicker();
+        $('.date-picker').datepicker("option", "dateFormat", "yy-mm-dd");
+    },
+
+    selectShow(value) {
+        let show = this.model.findBy('id', value);
+        if (show !== undefined) {
+            this.set('host', show.get('defaultHost'));
+            this.set('selectedShow', show);
+        }
+        else {
+            this.set('host', '');
+        }
+    },
+
+    createPlaylist() {
+
+        if (this.get('selectedShow') !== undefined &&
+            this.get('host') !== undefined && this.get('host') !== "" &&
+            this.get('date') !== undefined && this.get('date') !== "") {
+
+        }
+        else {
+            alert("no show");
+        }
+    },
+
+    sortedProperties: ['name'],
+    sortedShows: Ember.computed.sort('model', 'sortedProperties'),
+
+
+});
