dcefec49c0d132a9d06801b7ea407aeb2e9ca3d2
Michael Marner <michael@20papercups.net>
2016-06-06 17:19:39 +0930

    Stats graph working on show page.

--- frontend/app/components/bar-graph.js
+++ frontend/app/components/bar-graph.js
@@ -1,4 +1,43 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+    didInsertElement() {
+        Chart.defaults.global.responsive = true;
+        Chart.defaults.global.legend.display =false
+    },
+
+
+    didRender() {
+        let data = this.get('data');
+
+        let toGraph = {
+            labels: [],
+            datasets: [{
+                data: []
+            }]
+        }
+
+        data.forEach(item => {
+            toGraph.labels.push(item.get('name'));
+            toGraph.datasets[0].data.push(item.get('value'));
+        });
+
+        let options = {
+            type: 'bar', 
+            data: toGraph, 
+            options: {
+                scales: {
+                    yAxes: [
+                        { 
+                            ticks: {beginAtZero: true}
+                        }
+                    ]
+                }
+            }
+        }
+        console.log(this.elementId);
+        let ctx = this.$().children(".bar-chart").first();//.getContext('2d');
+        let statsChart = new Chart(ctx, options);
+    }
 });
--- frontend/app/components/playlist-new-form.js
+++ frontend/app/components/playlist-new-form.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+    tagName: 'canvas',
 
     didInsertElement() {
         $('.date-picker').datepicker();
