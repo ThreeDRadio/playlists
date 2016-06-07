7fa72ed56e01e0189b7cf422786c45bfedd6caa6
Michael Marner <michael@20papercups.net>
2016-06-07 12:00:13 +0930

    Add ability to edit playlist entries.
    * Pressing the edit button changes the track info to text boxes.
    * Pressing the new OK button saves the changes

--- frontend/app/components/playlist-edit-tablerow.js
+++ frontend/app/components/playlist-edit-tablerow.js
@@ -2,12 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     tagName: '',
+    editMode: false,
 
 
     actions: {
         deleteTrack() {
             let track = this.get('track');
             this.get('deleteAction')(track);
+        },
+
+
+        editTrack() {
+            this.set('editMode', true);
+        },
+
+        updateTrack() {
+            let track = this.get('track');
+            track.save();
+            this.set('editMode', false);
         }
     }
 });
