b16733726d157415339c7a9bd4b8806c70249980
Michael Marner <michael@20papercups.net>
2016-06-10 21:53:48 +0930

    Bootstrap-ise the login form.
    Still kind of clunky, maybe move up into nav bar.

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -1,18 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-    session: Ember.inject.service(),
+  session: Ember.inject.service(),
 
-    actions: {
-        authenticate() {
+  actions: {
+    authenticate() {
 
-            let {username, password } = this.getProperties('username', 'password');
-            console.log(username);
-            console.log(password);
-            this.get('session').authenticate('authenticator:django-rest', {identification:username, password:password}).catch((reason) => {
-                this.set('errorMessage', reason);
-            });
-        }
-
+      let {username, password } = this.getProperties('username', 'password');
+      this.get('session').authenticate('authenticator:django-rest', {identification:username, password:password}).catch((reason) => {
+      console.log(reason);
+        this.set('errorMessage', reason.non_field_errors);
+        this.set('username', '');
+        this.set('password', '');
+      });
     }
+
+  }
 });
