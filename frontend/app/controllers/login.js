9af2b90157241d25bec4105c2381ac3196c11622
Michael Marner <michael@20papercups.net>
2016-06-14 13:25:31 +0930

    Move action for edit/delete track from span to button.
    Edit and delete now work in Firefox.
    Fixes #14, #13

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -7,7 +7,9 @@ export default Ember.Controller.extend({
     authenticate() {
 
       let {username, password } = this.getProperties('username', 'password');
-      this.get('session').authenticate('authenticator:django-rest', {identification:username, password:password}).catch((reason) => {
+      this.get('session').authenticate('authenticator:django-rest', {identification:username, password:password}).then(() => {
+        this.get('session').data.user = this.store.find('user', 'me');
+      }).catch((reason) => {
         this.set('errors', reason);
         this.set('username', '');
         this.set('password', '');
