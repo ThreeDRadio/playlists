55d969e40f472c78985d31d498a5358b364b4337
Michael Marner <michael@20papercups.net>
2016-06-14 15:54:32 +0930

    Add CORSHeaders to middleware classes.

--- backend/logger/settings/base.py
+++ backend/logger/settings/base.py
@@ -31,6 +31,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
