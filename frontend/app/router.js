f5b8af28b0fb50e0fde6b11b7da7db3f732a5823
Michael Marner <michael@20papercups.net>
2016-06-15 10:55:24 +0930

    New route for today's plays.

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -8,11 +8,12 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('session', {resetNamespace: true}, function() {});
   this.route('playlists', function() {
-      this.route('index', {path: '/'} );
-      this.route('playlist', {path: '/:playlist_id' });
-      this.route('edit', {path: '/:playlist_id/edit'});
-      this.route('new');
-      this.route('show', {path: '/shows/:show_id'});
+    this.route('index', {path: '/'} );
+    this.route('playlist', {path: '/:playlist_id' });
+    this.route('edit', {path: '/:playlist_id/edit'});
+    this.route('new');
+    this.route('show', {path: '/shows/:show_id'});
+    this.route('today');
   });
   this.route('login');
 });
