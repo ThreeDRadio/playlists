7654b8fab761df739b47f3c5cacf3f81b14fea02
Michael Marner <michael@20papercups.net>
2016-06-15 12:05:45 +0930

    Display today's plays.

--- frontend/app/routes/playlists/today.js
+++ frontend/app/routes/playlists/today.js
@@ -1,4 +1,8 @@
 import Ember from 'ember';
+import ENV from '../../config/environment';
 
 export default Ember.Route.extend({
+  model: function() {
+    return Ember.$.getJSON(ENV.APP.API_HOST + "/api/playlistentries/today/");
+  }
 });
