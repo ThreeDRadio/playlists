23132602e91d42c965764bab4802a4a934e5e9b2
Michael Marner <michael@20papercups.net>
2016-06-17 13:50:19 +0930

    Unit tests for Playlist
    * Model exists
    * Incomplete by default
    * Has tracks relationship
    * Has parent show relationship

--- frontend/tests/unit/models/playlist-test.js
+++ frontend/tests/unit/models/playlist-test.js
@@ -2,11 +2,35 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('playlist', 'Unit | Model | playlist', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:playlistentry', 'model:show']
 });
 
 test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('Incomplete by default', function(assert) {
+  let model = this.subject();
+  // let store = this.store();
+  assert.equal(model.get('complete'), false);
+});
+
+test('has tracks', function(assert) {
+  let Playlist= this.store().modelFor('playlist');
+  let relationship = Ember.get(Playlist, 'relationshipsByName').get('tracks');
+
+  assert.equal(relationship.key, 'tracks');
+  assert.equal(relationship.kind, 'hasMany');
+  assert.equal(relationship.type, 'playlistentry');
+});
+
+test('has parent ', function(assert) {
+  let Playlist= this.store().modelFor('playlist');
+  let relationship = Ember.get(Playlist, 'relationshipsByName').get('show');
+
+  assert.equal(relationship.key, 'show');
+  assert.equal(relationship.kind, 'belongsTo');
+  assert.equal(relationship.type, 'show');
 });
