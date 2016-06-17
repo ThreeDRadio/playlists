4d93c2efa6a254675895753bbe552933be2d96d1
Michael Marner <michael@20papercups.net>
2016-06-17 13:59:47 +0930

    Unit tests for Show model.
    * Mostly just checking the relationships are setup right.

--- frontend/tests/unit/models/show-test.js
+++ frontend/tests/unit/models/show-test.js
@@ -2,11 +2,38 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('show', 'Unit | Model | show', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:playlist','model:TopArtist','model:statistic']
 });
 
 test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+
+test('has playlists', function(assert) {
+  let Show = this.store().modelFor('show');
+  let relationship = Ember.get(Show, 'relationshipsByName').get('playlists');
+
+  assert.equal(relationship.key, 'playlists');
+  assert.equal(relationship.kind, 'hasMany');
+  assert.equal(relationship.type, 'playlist');
+});
+
+test('has top artists', function(assert) {
+  let Show = this.store().modelFor('show');
+  let relationship = Ember.get(Show, 'relationshipsByName').get('topartists');
+
+  assert.equal(relationship.key, 'topartists');
+  assert.equal(relationship.kind, 'hasMany');
+  assert.equal(relationship.type, 'top-artist');
+});
+
+test('has statistics', function(assert) {
+  let Show = this.store().modelFor('show');
+  let relationship = Ember.get(Show, 'relationshipsByName').get('statistics');
+
+  assert.equal(relationship.key, 'statistics');
+  assert.equal(relationship.kind, 'hasMany');
+  assert.equal(relationship.type, 'statistic');
 });
