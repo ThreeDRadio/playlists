b0f1288fe15f8e91a16c20880c9f326628206f09
Michael Marner <michael@20papercups.net>
2016-06-17 13:07:45 +0930

    Proper unit tests for pretty duration.

--- frontend/tests/unit/helpers/pretty-duration-test.js
+++ frontend/tests/unit/helpers/pretty-duration-test.js
@@ -3,8 +3,28 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | pretty duration');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = prettyDuration([42]);
-  assert.ok(result);
+test('Removes zero hour part', function(assert) {
+  let result = prettyDuration(["00:12:53"]);
+  assert.equal(result, "12:53");
+
+  result = prettyDuration(["01:12:53"]);
+  assert.equal(result, "1:12:53");
+});
+
+test('Removes zero leading minute', function(assert) {
+  let result = prettyDuration(["00:02:53"]);
+  assert.equal(result, "2:53");
+});
+
+test('Displays single leading zero minute', function(assert) {
+  let result = prettyDuration(["00:00:53"]);
+  assert.equal(result, "0:53");
+});
+
+test("Doesn't mess with what it doesn't understand", function(assert) {
+  let result = prettyDuration(["Not a time at all"]);
+  assert.equal(result, "Not a time at all");
+
+  result = prettyDuration(["00: Just Kidding!"]);
+  assert.equal(result, "00: Just Kidding!");
 });
