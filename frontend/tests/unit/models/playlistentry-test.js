46407d8b420b3e9c62005425e56dd1fda9ad9b14
Michael Marner <michael@20papercups.net>
2016-06-17 13:30:55 +0930

    Unit test for playlist entry model.
    * Loads relationship with model correctly
    * Checks to see whether a default duration is created.

--- frontend/tests/unit/models/playlistentry-test.js
+++ frontend/tests/unit/models/playlistentry-test.js
@@ -2,11 +2,16 @@ import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('playlistentry', 'Unit | Model | playlistentry', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:playlist']
 });
 
 test('it exists', function(assert) {
   let model = this.subject();
   // let store = this.store();
   assert.ok(!!model);
+});
+test('Default duration', function(assert) {
+  let model = this.subject();
+  // let store = this.store();
+  assert.equal(model.get('duration'), "0:00");
 });
