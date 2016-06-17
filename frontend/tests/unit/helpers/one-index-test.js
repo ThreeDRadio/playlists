1aedb053cc935f475aebc3b4d09036ba30c23e0a
Michael Marner <michael@20papercups.net>
2016-06-17 12:39:18 +0930

    Actual unit tests for one-index helper.

--- frontend/tests/unit/helpers/one-index-test.js
+++ frontend/tests/unit/helpers/one-index-test.js
@@ -4,7 +4,25 @@ import { module, test } from 'qunit';
 module('Unit | Helper | one index');
 
 // Replace this with your real tests.
-test('Valid integers', function(assert) {
+test('one-index works with valid integers', function(assert) {
   let result = oneIndex([42]);
-  assert.ok(result);
+  assert.strictEqual(result, 43);
+
+  result = oneIndex([-3]);
+  assert.strictEqual(result, -2);
+
+  result = oneIndex([0]);
+  assert.strictEqual(result, 1);
+});
+
+test('one-index ignores what it does not understand', function(assert) {
+
+  let result = oneIndex(["here is a string"]);
+  assert.strictEqual(result, "here is a string");
+
+  result = oneIndex([null]);
+  assert.strictEqual(result, null);
+
+  result = oneIndex([""]);
+  assert.strictEqual(result, "");
 });
