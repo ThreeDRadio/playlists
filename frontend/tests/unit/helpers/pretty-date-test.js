c505dd72feadb6e2ffa7f6923f52ecca2c284e98
Michael Marner <michael@20papercups.net>
2016-06-17 13:24:56 +0930

    Unit test for pretty dates.
    Currently failing I think because of timezone weirdness in phantomjs.

--- frontend/tests/unit/helpers/pretty-date-test.js
+++ frontend/tests/unit/helpers/pretty-date-test.js
@@ -4,7 +4,13 @@ import { module, test } from 'qunit';
 module('Unit | Helper | pretty date');
 
 // Replace this with your real tests.
-test('it works', function(assert) {
-  let result = prettyDate([42]);
-  assert.ok(result);
+test('Makes valid dates pretty', function(assert) {
+  // fucking timezones
+  let date = new Date();
+  date.setDate(18);
+  date.setFullYear(2016);
+  date.setMonth(6);
+  let result = prettyDate(date);
+  assert.equal(result, "June 18, 2016");
 });
+
