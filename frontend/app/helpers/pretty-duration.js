69fc54c90a3455de6d11c87eb57e98e4207632be
Michael Marner <michael@20papercups.net>
2016-06-17 13:08:07 +0930

    More robust string matching using regex.

--- frontend/app/helpers/pretty-duration.js
+++ frontend/app/helpers/pretty-duration.js
@@ -1,15 +1,22 @@
 import Ember from 'ember';
 
 export function prettyDuration(params/*, hash*/) {
-    if (params[0].substr(0,3) !== "00:") {
-        return params[0];
-    }
-    else if (params[0].substr(0,4) === "00:0") {
-        return params[0].substr(4);
-    }
-    else {
-        return params[0].substr(3);
-    }
+
+  // if it contains 2 leading hour zeros
+  if (/^00:[1-9]{2}:\d{2}$/.test(params[0])) {
+    return params[0].substr(3);
+  }
+  // contains one digit of hours
+  else if (/^0[1-9]:\d{2}:\d{2}$/.test(params[0])) {
+    return params[0].substr(1);
+  }
+  // contains 0 or more minutes
+  else if (/^00:0\d:\d{2}$/.test(params[0])) {
+    return params[0].substr(4);
+  }
+  else {
+    return params[0];
+  }
 }
 
 export default Ember.Helper.helper(prettyDuration);
