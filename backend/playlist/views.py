a3cdd551f962bed7b9ea6dd91647470eeb86e6a2
Michael Marner <michael@20papercups.net>
2016-06-27 21:01:25 +0930

    Bring back playlist-as-text view.

--- backend/playlist/views.py
+++ backend/playlist/views.py
@@ -24,38 +24,6 @@ import logging
 
 
 # Create your views here.
-
-def index(request):
-    songCount = PlaylistEntry.objects.all().count()
-    local = PlaylistEntry.objects.filter(local=True).count()
-    australian = PlaylistEntry.objects.filter(australian=True).count()
-    female = PlaylistEntry.objects.filter(female=True).count()
-    artists = PlaylistEntry.objects.distinct('artist').count()
-    top = PlaylistEntry.objects.values('artist').annotate(plays=Count('artist')).order_by('-plays')[:10]
-
-    playlists = Playlist.objects.order_by('date').order_by('-pk')
-    if request.GET.get('saved') == 'true':
-        messages.success(request, 'Playlist Submitted.')
-    context = RequestContext(request, {
-        'playlists': playlists,
-        'songCount': songCount,
-        'artists': artists,
-        'local': local,
-        'australian': australian,
-        'female': female,
-        'top': top,
-    })
-    return render(request, 'playlist/index.html', context)
-
-
-def today(request):
-    songs = PlaylistEntry.objects.filter(playlist__date=date.today()).values('artist', 'title', 'album').annotate(plays=Count('title')).order_by('-plays')
-    context = RequestContext(request, {
-        'songs': songs,
-    })
-    return render(request, 'playlist/today.html', context)
-
-
 def summary(request):
     startDate = request.GET.get('startDate', date.min)
     endDate = request.GET.get('endDate', date.max)
@@ -96,28 +64,6 @@ def summary(request):
     return response
 
 
-def new(request):
-    if request.method == 'POST':
-        form = NewPlaylistForm(request.POST)
-        if form.is_valid():
-            playlist = form.save()
-            return HttpResponseRedirect('/logger/playlists/' + str(playlist.id) + '/edit/')
-        else:
-            context = RequestContext(request, {
-                'form': form,
-                'shows': Show.objects.all()
-            })
-            return render(request, 'playlist/new.html', context)
-
-    else:
-        form = NewPlaylistForm()
-        context = RequestContext(request, {
-            'form': form,
-            'shows': Show.objects.all()
-        })
-        return render(request, 'playlist/new.html', context)
-
-
 def playlist(request, playlist_id):
     playlist = get_object_or_404(Playlist, pk=playlist_id)
 
@@ -152,69 +98,6 @@ def playlist(request, playlist_id):
                 out.writerow([track.artist, track.title, track.album, track.local, track.australian, track.female,
                               track.newRelease])
             return response
-    return render(request, "playlist/view.html", context)
-
-
-def edit(request, playlist_id):
-    playlist = Playlist.objects.get(pk=playlist_id)
-
-    EntryFormSet = modelformset_factory(PlaylistEntry, extra=60, form=PlaylistEntryForm)
-    formset = EntryFormSet(queryset=PlaylistEntry.objects.filter(playlist=playlist))
-
-    if request.method == 'POST':
-        formset = EntryFormSet(request.POST)
-        if formset.is_valid():
-            playlistEntries = formset.save(commit=False)
-            for entry in playlistEntries:
-                entry.playlist = playlist
-                entry.save()
-
-            if request.POST.get('final'):
-                playlist.complete = True
-                playlist.save()
-                return HttpResponseRedirect('/logger/?saved=true')
-        else:
-            messages.error(request,
-                           'You have invalid data in your logging sheet. Please fix the problems and try saving again..')
-            context = RequestContext(request, {
-                'playlist': playlist,
-                'formset': formset,
-            })
-
-            return render(request, 'playlist/edit.html', context)
-
-        formset = EntryFormSet(queryset=PlaylistEntry.objects.filter(playlist=playlist))
-        messages.success(request, 'Playlist saved.')
-
-    context = RequestContext(request, {
-        'playlist': playlist,
-        'formset': formset,
-    })
-
-    return render(request, 'playlist/edit.html', context)
-
-
-def shows(request, show_id):
-    show = get_object_or_404(Show, pk=show_id)
-    playlists = show.playlists.order_by('-date')
-    songCount = PlaylistEntry.objects.filter(playlist__show=show).count()
-    local = PlaylistEntry.objects.filter(playlist__show=show).filter(local=True).count()
-    australian = PlaylistEntry.objects.filter(playlist__show=show).filter(australian=True).count()
-    female = PlaylistEntry.objects.filter(playlist__show=show).filter(female=True).count()
-    artists = PlaylistEntry.objects.filter(playlist__show=show).distinct('artist').count()
-    top = PlaylistEntry.objects.filter(playlist__show=show).values('artist').annotate(plays=Count('artist')).order_by('-plays')[:10]
-    context = RequestContext(request, {
-        'show': show,
-        'playlists': playlists,
-        'songCount': songCount,
-        'artists': artists,
-        'local': local,
-        'australian': australian,
-        'female': female,
-        'top': top,
-    })
-
-    return render(request, 'playlist/show.html', context)
 
 
 def reports(request):
