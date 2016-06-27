e9040a013885b6788a7c850564302c8002d1cdfd
Michael Marner <michael@20papercups.net>
2016-06-27 20:29:13 +0930

    Extend the default session service, add currentUser prop.
    * computed property to get the currently logged in user from the session.

---
+++ frontend/app/services/session.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import DS from 'ember-data';
+import ESASession from 'ember-simple-auth/services/session';
+
+export default ESASession.extend({
+
+  store: Ember.inject.service(),
+
+  currentUser: Ember.computed('isAuthenticated', function() {
+    if (this.get('isAuthenticated')) {
+      const promise = this.get('store').find('user', 'me');
+      return DS.PromiseObject.create({promise: promise});
+    }
+  })
+});
