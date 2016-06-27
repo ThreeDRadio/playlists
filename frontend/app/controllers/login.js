5e323c20fb34cbe6a08cc6c18e3c519177757cd8
Michael Marner <michael@20papercups.net>
2016-06-27 20:32:35 +0930

    Don't try and set the current user in login controller.
    Use session.currentUser computed property instead.

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -8,17 +8,12 @@ export default Ember.Controller.extend({
 
       let {username, password } = this.getProperties('username', 'password');
       this.get('session').authenticate('authenticator:django-rest', {identification:username, password:password}).then(() => {
-        this.get('session').data.user = this.store.find('user', 'me');
+        this.transitionToRoute('index');
       }).catch((reason) => {
         this.set('errors', reason);
         this.set('username', '');
         this.set('password', '');
       });
-
-      if (this.get('session').isAuthenticated) {
-        this.transitionToRoute('index');
-      }
     }
-
   }
 });
