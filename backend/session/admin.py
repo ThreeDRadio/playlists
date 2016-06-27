f54f2c5d490e1b2537ee2e8f50dfabdd199f666b
Michael Marner <michael@20papercups.net>
2016-06-28 09:24:26 +0930

    Make Whitelist appear in admin.

--- backend/session/admin.py
+++ backend/session/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from models import Whitelist
+
 # Register your models here.
+
+
+class WhitelistAdmin(admin.ModelAdmin):
+    model = Whitelist
+
+
+admin.site.register(Whitelist, WhitelistAdmin)
