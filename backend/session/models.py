0afdd77f4bf15cc858680fae352694476d72470e
Michael Marner <michael@20papercups.net>
2016-06-27 21:29:52 +0930

    Whitelist model.

--- backend/session/models.py
+++ backend/session/models.py
@@ -4,9 +4,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
-
-
 @receiver(post_save, sender = User)
 def init_new_user(sender, instance, signal, created, **kqargs):
     if created:
         Token.objects.create(user = instance)
+
+
+
+class Whitelist(models.Model):
+    ip = models.GenericIPAddressField()
+    name = models.CharField(max_length=200)
