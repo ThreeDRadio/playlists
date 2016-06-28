41dc4e7efdfb37a42e1067f1dc732a466afe34db
Michael Marner <michael@20papercups.net>
2016-06-28 15:47:13 +0930

    Add fullName computed property to user.

--- frontend/app/models/user.js
+++ frontend/app/models/user.js
@@ -3,9 +3,13 @@ import attr from 'ember-data/attr';
 
 export default Model.extend({
 
-    username: attr('string'),
-    firstName: attr('string'),
-    lastName: attr('string'),
-    email: attr('string'),
-  
+  username: attr('string'),
+  firstName: attr('string'),
+  lastName: attr('string'),
+  email: attr('string'),
+
+  fullName: Ember.computed('firstName', 'lastName', function() {
+    return `${this.get('firstName')} ${this.get('lastName')}`;
+  })
+
 });
