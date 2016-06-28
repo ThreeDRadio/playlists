194d9aa9cef85a6b6f3c6b950ba637ec7f9b6589
Michael Marner <michael@20papercups.net>
2016-06-28 12:46:58 +0930

    Authenticate the todays plays route.
    * This route is just using jquery, not ember data, so we need to manually add
    the authentication headers to the ajax request.

--- frontend/app/routes/playlists/today.js
+++ frontend/app/routes/playlists/today.js
@@ -2,7 +2,18 @@ import Ember from 'ember';
 import ENV from '../../config/environment';
 
 export default Ember.Route.extend({
-  model: function() {
-    return Ember.$.getJSON(ENV.APP.API_HOST + "/api/playlistentries/today/");
+
+  session: Ember.inject.service(),
+
+  model() {
+    let sesh = this.get('session');
+    sesh.authorize('authorizer:django-rest', (headerName, headerValue) => {
+      return Ember.$.getJSON({
+        url: ENV.APP.API_HOST + "/api/playlistentries/today/",
+        beforeSend: function(xhr) {
+          xhr.setRequestHeader(headerName, headerValue);
+        },
+      });
+    });
   }
 });
