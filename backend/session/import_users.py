620484497af9b0c8617fefea0844abaefe189399
Michael Marner <michael@20papercups.net>
2016-06-29 10:37:34 +0930

    Script for importing users from old table to Django
    * Creates Catalogue Editors group if it doesn't exist
    * Copies over all attributes from old table
    * Inserts users into editors group if needed
    * Inserts their old password into the OldPassword table.

--- backend/session/import_users.py
+++ backend/session/import_users.py
@@ -1,12 +1,39 @@
+from session.models import OldUser, OldPassword
+from django.contrib.auth.models import User, Group
+
+
+def createEditorsGroup():
+    if not Group.objects.filter(name="Catalogue Editors").exists():
+        print "Editors group not found, creating"
+        return Group.objects.create(name="Catalogue Editors")
+    else:
+        print "Using existing editors group"
+        return Group.objects.get(name="Catalogue Editors")
+
 def importUsers():
-    all_users = importer.models.User.objects.all()
+    editors = createEditorsGroup()
+    all_users = OldUser.objects.all()
     for u in all_users:
-        print (u.username)
-        newUser = User.objects.create_user(u.username)
+        if User.objects.filter(username=u.username).exists():
+            newUser = User.objects.get(username=u.username)
+        else:
+            newUser = User.objects.create_user(u.username)
         if u.first is not None:
             newUser.first_name = u.first
-            newUser.save()
         if u.last is not None:
             newUser.last_name = u.last
-            newUser.save()
+        if u.admin == True:
+            newUser.is_staff = True
+        if u.cdeditor == True:
+            editors.user_set.add(newUser)
+        if u.active == True:
+            newUser.is_active = True
+        else:
+            newUser.is_active = False
+        if u.password is not None:
+            OldPassword.objects.filter(user=newUser).delete()
+            OldPassword.objects.create(user=newUser, password=u.password)
+        newUser.save()
+    
 
+importUsers()
