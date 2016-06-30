c55d8e31c6ac9efa5e9f2ecc7689e98339c144f4
Michael Marner <michael@20papercups.net>
2016-06-30 21:31:42 +0930

    Remove table of releases to its own component.

--- frontend/app/routes/catalogue/latest.js
+++ frontend/app/routes/catalogue/latest.js
@@ -7,12 +7,12 @@ export default Ember.Route.extend({
     }
   }, 
 
-  model() {
+  model(params) {
     let now = new Date();
     let twoMonthsAgo = new Date(now.getFullYear(), 
                                 now.getMonth() - 2, 
                                 now.getDate());
-    console.log(twoMonthsAgo);
+
     return this.store.query('release', {ordering: '-arrivaldate,artist,title', min_arrival: moment(twoMonthsAgo).format('YYYY-MM-DD')});
   },
 
